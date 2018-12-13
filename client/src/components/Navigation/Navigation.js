e7a0f562388359742f7456d0c7de8a9a6eb4ad12
Adam P <adamparalovos@hotmail.com>
2018-12-12 20:43:55 -0500

    Updated home page

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -9,38 +9,31 @@ class Navigation extends Component {
 
     return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-        <a className="navbar-brand" href="/">
-          <div id="logo">
-            {" "}
-            <img src={logo} alt="logo" />{" "}
-            <span className="title">DataSense</span>
-          </div>
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbar"
-        >
+        {this.props.isLoggedIn ? (
+          <a className="navbar-brand" href="/dashboard">
+            <div id="logo">
+              {" "}
+              <img src={logo} alt="logo" /> <span className="title">DataSense</span>
+            </div>
+          </a>
+        ) : (
+          <a className="navbar-brand" href="/">
+            <div id="logo">
+              {" "}
+              <img src={logo} alt="logo" /> <span className="title">DataSense</span>
+            </div>
+          </a>
+        )}
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar">
           <span className="navbar-toggler-icon" />
         </button>
 
-        <div
-          className="collapse navbar-collapse justify-content-end"
-          id="navbar"
-        >
+        <div className="collapse navbar-collapse justify-content-end" id="navbar">
           <ul className="navbar-nav mt-2 mt-lg-0">
             {this.props.isLoggedIn ? (
               <React.Fragment>
                 <li className="nav-item">
-                  <a
-                    className={
-                      this.props.path === "/dashboard"
-                        ? activeLinkClasses
-                        : inactiveLinkClasses
-                    }
-                    href="/dashboard"
-                  >
+                  <a className={this.props.path === "/dashboard" ? activeLinkClasses : inactiveLinkClasses} href="/dashboard">
                     <i className="fas fa-chart-line" /> Dashboard{" "}
                   </a>
                 </li>
@@ -53,26 +46,12 @@ class Navigation extends Component {
             ) : (
               <React.Fragment>
                 <li className="nav-item">
-                  <a
-                    className={
-                      this.props.path === "/"
-                        ? activeLinkClasses
-                        : inactiveLinkClasses
-                    }
-                    href="/"
-                  >
+                  <a className={this.props.path === "/" ? activeLinkClasses : inactiveLinkClasses} href="/">
                     <i className="fas fa-home" /> Home{" "}
                   </a>
                 </li>
                 <li className="nav-item">
-                  <a
-                    className={
-                      this.props.path === "/login"
-                        ? activeLinkClasses
-                        : inactiveLinkClasses
-                    }
-                    href="/login"
-                  >
+                  <a className={this.props.path === "/login" ? activeLinkClasses : inactiveLinkClasses} href="/login">
                     <i className="fas fa-sign-in-alt" /> Login/Register
                   </a>
                 </li>
